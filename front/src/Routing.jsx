f9319df3f8ba1f0b8556253a0b6cbdd4fadb5e4e
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2022-12-19 16:26:21 +0500

    add front

---
+++ front/src/Routing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Routes , Route} from 'react-router-dom'
+import HomePage from './pages/homePage/HomePage'
+import NewsPage from './pages/newsPage/NewsPage'
+
+function Routing() {
+  return (
+        <>
+            <Routes>
+                <Route path='/'  element= { <HomePage />  }/>
+                <Route path='/news'  element= { <NewsPage />  }/>
+
+            </Routes>
+        </>
+    )
+}
+
+export default Routing
