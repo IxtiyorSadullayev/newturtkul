5b8a5d0619e3dcb855df8371164eacc9aaa4bc73
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2022-12-22 16:14:57 +0500

    add ofy

---
+++ front/src/pages/homePage/OFYlar/Ofy.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Ofy  = () =>{
+    return (
+        <div className="container">
+            <div className="asosiy">
+                <h1>To'rtko'l tumanining bo'limlari</h1>
+                <p>Barcha bo'limlar bitta sahifada</p>
+            </div>
+        </div>
+    )
+}
+
+export default Ofy;
