3f329c586ce9025a4dbb55f731708ccc5bd0d3c9
Axmedov0649 <mineflayerminecraft@gmail.com>
2022-12-25 14:43:39 +0500

    aaa

--- front/src/Routing.jsx
+++ front/src/Routing.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {Routes , Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homePage/HomePage'
 import NewsPage from './pages/newsPage/NewsPage'
 
 function Routing() {
-  return (
+    return (
         <>
             <Routes>
-                <Route path='/'  element= { <HomePage />  }/>
-                <Route path='/news'  element= { <NewsPage />  }/>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/news' element={<NewsPage />} />
             </Routes>
         </>
     )
--- front/src/index.js
+++ front/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <NewsPage />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
